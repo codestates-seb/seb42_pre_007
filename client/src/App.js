85c6a88b90414e5829fa4fdd00d279a244214e6a
ofcpire <ofcpire@gmail.com>
2023-02-16 15:24:12 +0900

    Chore : axios, globalStyle 사용

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,13 @@
-import './App.css';
+import "./App.css";
+import { GlobalStyle } from "./globalStyle";
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <GlobalStyle />
+      hello world!
+    </div>
+  );
 }
 
 export default App;
