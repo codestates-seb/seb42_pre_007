d98ac3a94b034c7f44b04acedfe5a4d76f9068c8
GoF9490 <osis9490@gmail.com>
2023-02-19 23:39:28 +0900

    feat : CustomAuthorityUtils , UserDetailsServiceImpl , exception

---
+++ server/src/main/java/com/pre007/server/auth/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,77 @@
+package com.pre007.server.auth.userdetails;
+
+import com.pre007.server.auth.authorityutils.CustomAuthorityUtils;
+import com.pre007.server.exception.BusinessLogicException;
+import com.pre007.server.exception.ExceptionCode;
+import com.pre007.server.user.entity.User;
+import com.pre007.server.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import java.util.Collection;
+import java.util.Optional;
+
+@Component
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+    private final CustomAuthorityUtils authorityUtils;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> optionalUser = userRepository.findByEmail(username);
+        User findUser = optionalUser.orElseThrow(() ->
+                new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        return new UserDetailsImpl(findUser);
+    }
+
+    /**
+     * 클래스 네이밍이 좀 별로인듯. 적당한게 없을까?
+     */
+    private final class UserDetailsImpl extends User implements UserDetails {
+
+        // 리팩토링 필요 (entity에 setter 사용 배제시)
+        UserDetailsImpl(User user) {
+            setUserId(user.getUserId());
+            setEmail(user.getEmail());
+            setPassword(user.getPassword());
+            //setRoles();
+        }
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            return null;
+        }
+
+        @Override
+        public String getUsername() {
+            return getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return true;
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    }
+
+}
