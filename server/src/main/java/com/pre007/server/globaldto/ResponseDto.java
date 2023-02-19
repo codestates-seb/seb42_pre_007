13dddd8454609797aad2f0f7a9a1984d6db25a53
GoF9490 <osis9490@gmail.com>
2023-02-19 14:17:15 +0900

    test : UserController createSuccess

---
+++ server/src/main/java/com/pre007/server/globaldto/ResponseDto.java
@@ -0,0 +1,16 @@
+package com.pre007.server.globaldto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+public class ResponseDto<T> {
+
+    private T data;
+    private Integer code;
+
+    public ResponseDto(T data, Integer code) {
+        this.data = data;
+        this.code = code;
+    }
+}
