4896dfa60d7bb86aac60c58a6d1e2c13921c25a2
yjkim <k.yeojin15@gmail.com>
2023-02-21 17:53:51 +0900

    Feat: 로그인 기능 추가

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,40 +1,58 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { LogoImg,AuthFormWrap,AuthForm,AuthFormBox,AuthFormLabel,AuthFormInput,BlueButton,AuthLinkTo } from '../styles/authform'
+import axios  from 'axios';
 
 const Login = () => {
   const navigate = useNavigate();
-  const [ loginEmail, setLoginEmail ] = useState("");
-  const [ loginPw, setLoginPw ] = useState("");
+  const URI = process.env.REACT_APP_API_URI
+
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
+
   const handleLoginEmail = (e)=>{
     let content = e.target.value;
-    setLoginEmail(content)
+    setEmail(content)
   }
   const handleLoginPw = (e)=>{
     let content = e.target.value;
-    setLoginPw(content)
+    setPassword(content)
+  }
+
+  const POSTLogin = async(e)=>{
+    e.preventDefault()
+    axios({
+      method: 'post',
+      url: `${URI}`,
+      params: {
+        email,
+        password
+      },
+      data: {
+        email,
+        password
+      }
+    })
   }
 
   return (
-    <>
-      <AuthFormWrap>
-        <LogoImg/>
-        <AuthForm action='' method='POST'>
-          <AuthFormBox>
-            <AuthFormLabel htmlFor='loginEmail'>Email</AuthFormLabel>
-            <AuthFormInput type='text' title='loginEmail' id='loginEmail' value={loginEmail} onChange={handleLoginEmail} required />
-          </AuthFormBox>
-          <AuthFormBox>
-            <AuthFormLabel htmlFor='loginPw'>Password</AuthFormLabel>
-            <AuthFormInput type='password' title='loginPw' id='loginPw' value={loginPw} onChange={handleLoginPw} required />
-          </AuthFormBox>
-          <AuthFormBox>
-            <BlueButton>Log in</BlueButton>
-          </AuthFormBox>
-        </AuthForm>
-        <AuthLinkTo>Don’t have an account? <span onClick={()=>navigate('/users/signup')}>Sign up</span></AuthLinkTo>
-      </AuthFormWrap>
-    </>
+    <AuthFormWrap>
+      <LogoImg/>
+      <AuthForm>
+        <AuthFormBox>
+          <AuthFormLabel htmlFor='loginEmail'>Email</AuthFormLabel>
+          <AuthFormInput type='text' title='loginEmail' id='loginEmail' value={email} onChange={handleLoginEmail} required />
+        </AuthFormBox>
+        <AuthFormBox>
+          <AuthFormLabel htmlFor='loginPw'>Password</AuthFormLabel>
+          <AuthFormInput type='password' title='loginPw' id='loginPw' value={password} onChange={handleLoginPw} required />
+        </AuthFormBox>
+        <AuthFormBox>
+          <BlueButton onClick={POSTLogin}>Log in</BlueButton>
+        </AuthFormBox>
+      </AuthForm>
+      <AuthLinkTo>Don’t have an account? <span onClick={()=>navigate('/users/signup')}>Sign up</span></AuthLinkTo>
+    </AuthFormWrap>
   )
 }
 
