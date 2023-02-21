0c8de4160edfde5908d48cf52499b80bd73197b5
yjkim <k.yeojin15@gmail.com>
2023-02-21 17:55:19 +0900

    Feat: 회원가입 기능 추가

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,50 +1,69 @@
 import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { LogoImg,AuthFormWrap,AuthForm,AuthFormBox,AuthFormLabel,AuthFormInput,BlueButton,AuthLinkTo } from '../styles/authform'
+import axios  from 'axios';
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const [ signName, setSignName ] = useState("");
-  const [ signEmail, setSignEmail ] = useState("");
-  const [ signPw, setSignPw ] = useState("");
+  const URI = process.env.REACT_APP_API_URI
+
+  const [ displayName, setDisplayName ] = useState("");
+  const [ email, setEmail ] = useState("");
+  const [ password, setPassword ] = useState("");
 
   const handleSignName=(e)=>{
     let content = e.target.value;
-    setSignName(content)
+    setDisplayName(content)
   }
   const handleSignEmail=(e)=>{
     let content = e.target.value;
-    setSignEmail(content)
+    setEmail(content)
   }
   const handleSignPw=(e)=>{
     let content = e.target.value;
-    setSignPw(content)
+    setPassword(content)
+  }
+
+  const POSTSignUp = async(e)=>{
+    e.preventDefault()
+    await axios({
+      method: 'post',
+      url: `${URI}`,
+      params: {
+        displayName,
+        email,
+        password
+      },
+      data: {
+        displayName,
+        email,
+        password
+      }
+    })
   }
 
   return (
-    <>
-      <AuthFormWrap>
-        <LogoImg/>
-        <AuthForm action='' method='POST'>
-          <AuthFormBox>
-            <AuthFormLabel htmlFor='signUpName'>Display name</AuthFormLabel>
-            <AuthFormInput type='text' title='signUpName' id='signUpName' value={signName} onChange={handleSignName}  required/>
-          </AuthFormBox>
-          <AuthFormBox>
-            <AuthFormLabel htmlFor='signUpEmail'>Email</AuthFormLabel>
-            <AuthFormInput type='text' title='signUpEmail' id='signUpEmail' value={signEmail} onChange={handleSignEmail} required />
-          </AuthFormBox>
-          <AuthFormBox>
-            <AuthFormLabel htmlFor='signUpPw'>Password</AuthFormLabel>
-            <AuthFormInput type='password' title='signUpPw' id='signUpPw' value={signPw} onChange={handleSignPw} required/>
-          </AuthFormBox>
-          <AuthFormBox>
-            <BlueButton>Sign up</BlueButton>
-          </AuthFormBox>
-        </AuthForm>
-        <AuthLinkTo>Already have an account? <span onClick={()=>navigate('/users/login')}>Log in</span></AuthLinkTo>
-      </AuthFormWrap>
-    </>
+    <AuthFormWrap>
+      <LogoImg/>
+      <AuthForm>
+        <AuthFormBox>
+          <AuthFormLabel htmlFor='signUpName'>Display name</AuthFormLabel>
+          <AuthFormInput type='text' title='signUpName' id='signUpName' value={displayName} onChange={handleSignName}  required/>
+        </AuthFormBox>
+        <AuthFormBox>
+          <AuthFormLabel htmlFor='signUpEmail'>Email</AuthFormLabel>
+          <AuthFormInput type='text' title='signUpEmail' id='signUpEmail' value={email} onChange={handleSignEmail} required />
+        </AuthFormBox>
+        <AuthFormBox>
+          <AuthFormLabel htmlFor='signUpPw'>Password</AuthFormLabel>
+          <AuthFormInput type='password' title='signUpPw' id='signUpPw' value={password} onChange={handleSignPw} required/>
+        </AuthFormBox>
+        <AuthFormBox>
+          <BlueButton onClick={POSTSignUp}>Sign up</BlueButton>
+        </AuthFormBox>
+      </AuthForm>
+      <AuthLinkTo>Already have an account? <span onClick={()=>navigate('/users/login')}>Log in</span></AuthLinkTo>
+    </AuthFormWrap>
   )
 }
 
