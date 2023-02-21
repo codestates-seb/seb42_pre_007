1af81aa3361e57e0771319c066778cc2729e8034
yjkim <k.yeojin15@gmail.com>
2023-02-21 13:49:04 +0900

    Feat: SignUp.js State 추가

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,21 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { LogoImg,AuthFormWrap,AuthForm,AuthFormBox,AuthFormLabel,AuthFormInput,BlueButton,AuthLinkTo } from '../styles/authform'
 
 const Login = () => {
   const navigate = useNavigate();
+  const [ loginEmail, setLoginEmail ] = useState("");
+  const [ loginPw, setLoginPw ] = useState("");
+  const handleLoginEmail = (e)=>{
+    let content = e.target.value;
+    setLoginEmail(content)
+  }
+  const handleLoginPw = (e)=>{
+    let content = e.target.value;
+    setLoginPw(content)
+  }
+
   return (
     <>
       <AuthFormWrap>
         <LogoImg/>
-        <AuthForm action='' method=''>
+        <AuthForm action='' method='POST'>
           <AuthFormBox>
             <AuthFormLabel htmlFor='loginEmail'>Email</AuthFormLabel>
-            <AuthFormInput type='text' title='loginEmail' id='loginEmail' />
+            <AuthFormInput type='text' title='loginEmail' id='loginEmail' value={loginEmail} onChange={handleLoginEmail} required />
           </AuthFormBox>
           <AuthFormBox>
             <AuthFormLabel htmlFor='loginPw'>Password</AuthFormLabel>
-            <AuthFormInput type='password' title='loginPw' id='loginPw' />
+            <AuthFormInput type='password' title='loginPw' id='loginPw' value={loginPw} onChange={handleLoginPw} required />
           </AuthFormBox>
           <AuthFormBox>
             <BlueButton>Log in</BlueButton>
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,25 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { LogoImg,AuthFormWrap,AuthForm,AuthFormBox,AuthFormLabel,AuthFormInput,BlueButton,AuthLinkTo } from '../styles/authform'
 
 const SignUp = () => {
   const navigate = useNavigate();
+  const [ signName, setSignName ] = useState("");
+  const [ signEmail, setSignEmail ] = useState("");
+  const [ signPw, setSignPw ] = useState("");
+
+  const handleSignName=(e)=>{
+    let content = e.target.value;
+    setSignName(content)
+  }
+  const handleSignEmail=(e)=>{
+    let content = e.target.value;
+    setSignEmail(content)
+  }
+  const handleSignPw=(e)=>{
+    let content = e.target.value;
+    setSignPw(content)
+  }
+
   return (
     <>
       <AuthFormWrap>
         <LogoImg/>
-        <AuthForm action='' method=''>
+        <AuthForm action='' method='POST'>
           <AuthFormBox>
             <AuthFormLabel htmlFor='signUpName'>Display name</AuthFormLabel>
-            <AuthFormInput type='text' title='signUpName' id='signUpName' />
+            <AuthFormInput type='text' title='signUpName' id='signUpName' value={signName} onChange={handleSignName}  required/>
           </AuthFormBox>
           <AuthFormBox>
             <AuthFormLabel htmlFor='signUpEmail'>Email</AuthFormLabel>
-            <AuthFormInput type='text' title='signUpEmail' id='signUpEmail' />
+            <AuthFormInput type='text' title='signUpEmail' id='signUpEmail' value={signEmail} onChange={handleSignEmail} required />
           </AuthFormBox>
           <AuthFormBox>
             <AuthFormLabel htmlFor='signUpPw'>Password</AuthFormLabel>
-            <AuthFormInput type='password' title='signUpPw' id='signUpPw' />
+            <AuthFormInput type='password' title='signUpPw' id='signUpPw' value={signPw} onChange={handleSignPw} required/>
           </AuthFormBox>
           <AuthFormBox>
             <BlueButton>Sign up</BlueButton>
