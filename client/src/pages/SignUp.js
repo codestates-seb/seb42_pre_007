0fe0147f00f8b5e0d6092c073ecb7b75a43f6478
yjkim <k.yeojin15@gmail.com>
2023-02-22 10:41:57 +0900

    Feat: 회원가입 기능 확인 완료

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,70 +1,99 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import { LogoImg,AuthFormWrap,AuthForm,AuthFormBox,AuthFormLabel,AuthFormInput,BlueButton,AuthLinkTo } from '../styles/authform'
-import axios  from 'axios';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import {
+  LogoImg,
+  AuthFormWrap,
+  AuthForm,
+  AuthFormBox,
+  AuthFormLabel,
+  AuthFormInput,
+  BlueButton,
+  AuthLinkTo,
+} from '../styles/authform';
+import axios from 'axios';
 
 const SignUp = () => {
   const navigate = useNavigate();
-  const URI = process.env.REACT_APP_API_URI
+  const URI = process.env.REACT_APP_API_URI;
 
-  const [ displayName, setDisplayName ] = useState("");
-  const [ email, setEmail ] = useState("");
-  const [ password, setPassword ] = useState("");
+  const [displayName, setDisplayName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSignName=(e)=>{
+  const handleSignName = (e) => {
     let content = e.target.value;
-    setDisplayName(content)
-  }
-  const handleSignEmail=(e)=>{
+    setDisplayName(content);
+  };
+  const handleSignEmail = (e) => {
     let content = e.target.value;
-    setEmail(content)
-  }
-  const handleSignPw=(e)=>{
+    setEmail(content);
+  };
+  const handleSignPw = (e) => {
     let content = e.target.value;
-    setPassword(content)
-  }
+    setPassword(content);
+  };
 
-  const POSTSignUp = async(e)=>{
-    e.preventDefault()
+  const POSTSignUp = async (e) => {
+    e.preventDefault();
     await axios({
       method: 'post',
-      url: `${URI}`,
-      params: {
-        displayName,
-        email,
-        password
-      },
+      url: `${URI}/users/signup`,
+      params: {},
       data: {
         displayName,
         email,
-        password
-      }
-    })
-  }
+        password,
+      },
+    });
+  };
 
   return (
     <AuthFormWrap>
-      <LogoImg/>
+      <LogoImg />
       <AuthForm>
         <AuthFormBox>
           <AuthFormLabel htmlFor='signUpName'>Display name</AuthFormLabel>
-          <AuthFormInput type='text' title='signUpName' id='signUpName' value={displayName} onChange={handleSignName}  required/>
+          <AuthFormInput
+            type='text'
+            title='signUpName'
+            id='signUpName'
+            value={displayName}
+            onChange={handleSignName}
+            required
+          />
         </AuthFormBox>
         <AuthFormBox>
           <AuthFormLabel htmlFor='signUpEmail'>Email</AuthFormLabel>
-          <AuthFormInput type='text' title='signUpEmail' id='signUpEmail' value={email} onChange={handleSignEmail} required />
+          <AuthFormInput
+            type='text'
+            title='signUpEmail'
+            id='signUpEmail'
+            value={email}
+            onChange={handleSignEmail}
+            required
+          />
         </AuthFormBox>
         <AuthFormBox>
           <AuthFormLabel htmlFor='signUpPw'>Password</AuthFormLabel>
-          <AuthFormInput type='password' title='signUpPw' id='signUpPw' value={password} onChange={handleSignPw} required/>
+          <AuthFormInput
+            type='password'
+            title='signUpPw'
+            id='signUpPw'
+            value={password}
+            onChange={handleSignPw}
+            required
+          />
         </AuthFormBox>
         <AuthFormBox>
           <BlueButton onClick={POSTSignUp}>Sign up</BlueButton>
         </AuthFormBox>
       </AuthForm>
-      <AuthLinkTo>Already have an account? <span onClick={()=>navigate('/users/login')}>Log in</span></AuthLinkTo>
+      <AuthLinkTo>
+        Already have an account?{' '}
+        <span onClick={() => navigate('/users/login')}>Log in</span>
+      </AuthLinkTo>
     </AuthFormWrap>
-  )
-}
+  );
+};
 
-export default SignUp
+export default SignUp;
